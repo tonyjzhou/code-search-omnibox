fd6058c958db3d4b7eb7ff84f01ebfaba8487f4c
Tony Zhou <tzhou@twitter.com>
2016-08-03 11:15:36 -0700

    REFACTOR: extract matchedWord and URI

--- background.js
+++ background.js
@@ -2,25 +2,27 @@ var omnibox = chrome.omnibox;
 var rootUrl = "https://phabricator-staging.twitter.biz";
 //var rootUrl = "https://phabricator.twitter.biz";
 
-omnibox.onInputChanged.addListener(function(text, suggest) {
-    $.get(rootUrl + "/typeahead/class/PhabricatorSearchDatasource/?__ajax__=true&q=" + text, 
-      function(data) {
-        var payload = $.parseJSON(data.substring(data.indexOf('{')))['payload'];
-        var suggestions = payload.map(
-          function(result) {
-            return {
-              content: rootUrl + result[1],
-              description: result.join(' ')
-            }
-          });
-        suggest(suggestions);
-      }).fail(function(error) {
+omnibox.onInputChanged.addListener(function (text, suggest) {
+    $.get(rootUrl + "/typeahead/class/PhabricatorSearchDatasource/?__ajax__=true&q=" + text,
+        function (data) {
+            var payload = $.parseJSON(data.substring(data.indexOf('{')))['payload'];
+            var suggestions = payload.map(
+                function (result) {
+                    var matchedWord = result[0];
+                    var URI = result[1];
+                    return {
+                        content: rootUrl + URI,
+                        description: matchedWord + ' ' + URI
+                    }
+                });
+            suggest(suggestions);
+        }).fail(function (error) {
         alert("Error: " + JSON.stringify(error));
-      });
+    });
 });
 
-omnibox.onInputEntered.addListener(function(url) {
-  chrome.tabs.update({
-    url: url
-  });
+omnibox.onInputEntered.addListener(function (url) {
+    chrome.tabs.update({
+        url: url
+    });
 });
