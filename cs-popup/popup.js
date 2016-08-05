c8396cb598771a122f19b619fe00718a0c33461b
Patrick Stover <pstover@twitter.com>
2016-08-05 09:45:28 -0700

    Use Twitter OS typeahead in cs-popup

--- cs-popup/popup.js
+++ cs-popup/popup.js
@@ -1,6 +1,9 @@
 var rootUrl = "https://phabricator-staging.twitter.biz/";
 var symbolSearch = rootUrl + "diffusion/symbol/";
 
+var language = "java";
+var typeaheadSearch = rootUrl + "typeahead/class/DiffusionSymbolByLanguageDatasource/?__ajax__=true%LANGUAGE&raw=%QUERY";
+
 function generateLangUrlParam() {
   return document.getElementById('language').value == "all" ? "" : "/?lang=" + document.getElementById('language').value;
 }
@@ -15,3 +18,69 @@ document.addEventListener('DOMContentLoaded', function () {
     if(e.keyCode==13) clickHandler();
   });
 })
+
+function prepareAsText(query, settings) {
+//  settings = prepareByWildcard(query, settings);
+  var language = document.getElementById('language').value;
+
+  settings.url = settings.url.replace('%QUERY', query).replace('%LANGUAGE', language == 'all' ? '' : '&q=' + language);
+  settings.dataType = 'text';
+
+  return settings;
+}
+
+function formatResponse(response) {
+  var payload = $.parseJSON(response.substring(response.indexOf('{')))['payload'];
+  var results = payload.map(
+    function (result) {
+
+      return {
+        value: result[0],
+        url: result[1],
+        type: result[5],
+        formatted: result[0] + ': ' + result[5]
+      }
+    });
+
+  return results;
+}
+
+var bestPictures = new Bloodhound({
+  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+//  prefetch: 'https://twitter.github.io/typeahead.js/data/films/post_1960.json',
+  remote: {
+//    url: 'https://twitter.github.io/typeahead.js/data/films/queries/%QUERY.json',
+    url: typeaheadSearch,
+    prepare: prepareAsText,
+//    wildcard: '%QUERY',
+    transform: formatResponse
+  }
+});
+
+function suggest(datum) {
+//  return '<div><strong>' + datum.value + ' - ' datum.url + '</div>';
+  return "hi";
+}
+
+$('#remote .typeahead').typeahead(null, {
+  name: 'best-pictures',
+  display: 'formatted',
+  source: bestPictures,
+//  templates: {
+//    empty: [
+//      '<div class="empty-message">',
+//        'unable to find any Best Picture winners that match the current query',
+//      '</div>'
+//    ].join('\n'),
+//    suggestion: Handlebars.compile('<div><strong>{{value}}</strong> – {{url}}</div>')
+//  }
+//  templates: {
+//    empty: [
+//     '<div class="empty-message">',
+//       'unable to find any Best Picture winners that match the current query',
+//     '</div>'
+//   ].join('\n'),
+//   suggestion: suggest
+// }
+});
